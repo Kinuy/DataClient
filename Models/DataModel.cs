fbbc8b06001e33cb849b00ddc6be7421a156975d
Tobias Weber <toby-weber@gmx.de>
2025-01-28 09:12:37 +0100

    add data model

---
+++ Models/DataModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataClient.Models;
+
+public class DataModel
+{
+    public string CountryName {  get; set; }
+    public int CaseCount {  get; set; }
+    public string FlagUri { get; set; }
+}
